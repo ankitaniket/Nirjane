2e6130c02cbffa57935e5775c4505ceba4654da8
ankitaniket <ankitaniket3@gmail.com>
2023-07-08 14:13:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send ('API Running'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,() => console.log( `Server is running on port ${PORT}`));
